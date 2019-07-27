2f1124e6c7285b6603f68dc004152fee6c8c953e
0xd34df00d <0xd34df00d@gmail.com>
2019-07-27 14:24:31 -0400

    Uniform style for it-labels

--- test/Spec.hs
+++ test/Spec.hs
@@ -11,16 +11,16 @@ main = hspec $ do
   let baseFooEnv = FooEnv 10 "meh"
   let baseBarEnv = BarEnv 4.2 [1, 2, 3]
   describe "Basic Has instance" $
-    it "Any type Has itself" $ do
+    it "any type Has itself" $ do
       let exFoo = extract baseFooEnv
       exFoo `shouldBe` baseFooEnv
   describe "Generic Has instances" $ do
-    it "Envs have their components" $ do
+    it "envs have their components" $ do
       let exFoo = extract $ AppEnv baseFooEnv baseBarEnv
       exFoo `shouldBe` baseFooEnv
       let exBar = extract $ AppEnv baseFooEnv baseBarEnv
       exBar `shouldBe` baseBarEnv
-    it "Tuples have their components" $ do
+    it "tuples have their components" $ do
       let exFoo = extract (baseFooEnv, baseBarEnv)
       exFoo `shouldBe` baseFooEnv
       let exBar = extract (baseFooEnv, baseBarEnv)
