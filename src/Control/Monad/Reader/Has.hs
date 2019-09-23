1d52693af165998a34ff62c4d0a01eb011ebaf5a
0xd34df00d <0xd34df00d@gmail.com>
2019-09-22 20:37:22 -0400

    Move Path definition to a separate header

--- src/Control/Monad/Reader/Has.hs
+++ src/Control/Monad/Reader/Has.hs
@@ -81,14 +81,7 @@ module Control.Monad.Reader.Has
 import Data.Proxy
 import GHC.Generics
 
-data Path = L Path | R Path | Here deriving (Show)
-data MaybePath = NotFound | Conflict | Found Path deriving (Show)
-
-type family Combine p1 p2 where
-  Combine ('Found path) 'NotFound = 'Found ('L path)
-  Combine 'NotFound ('Found path) = 'Found ('R path)
-  Combine 'NotFound 'NotFound = 'NotFound
-  Combine _ _ = 'Conflict
+import Data.Path
 
 type family Search part (g :: k -> *) :: MaybePath where
   Search part (K1 _ part) = 'Found 'Here
---
+++ src/Data/Path.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE DataKinds, TypeFamilies #-}
+
+module Data.Path where
+
+data Path = L Path | R Path | Here deriving (Show)
+data MaybePath = NotFound | Conflict | Found Path deriving (Show)
+
+type family Combine p1 p2 where
+  Combine ('Found path) 'NotFound = 'Found ('L path)
+  Combine 'NotFound ('Found path) = 'Found ('R path)
+  Combine 'NotFound 'NotFound = 'NotFound
+  Combine _ _ = 'Conflict
