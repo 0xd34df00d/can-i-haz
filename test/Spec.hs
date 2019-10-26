80021aafb4892079e2ce59579e712f0971e26575
0xd34df00d <0xd34df00d@gmail.com>
2019-10-26 18:28:31 -0400

    Update tests to cover the `update` function

--- test/Spec.hs
+++ test/Spec.hs
@@ -23,26 +23,43 @@ shouldNotTypecheck a = do
                                  then pure ()
                                  else throwIO e
 
+fooEnvTwice :: FooEnv -> FooEnv
+fooEnvTwice (FooEnv n s) = FooEnv (n * 2) (s <> s)
+
+barEnvCons :: BarEnv -> BarEnv
+barEnvCons (BarEnv d xs) = BarEnv d (0 : xs)
+
 main :: IO ()
 main = hspec $ do
   let baseFooEnv = FooEnv 10 "meh"
+  let baseFooEnvTwice = FooEnv 20 "mehmeh"
   let baseBarEnv = BarEnv 4.2 [1, 2, 3]
+  let baseBarEnvCons = BarEnv 4.2 [0, 1, 2, 3]
   describe "Has" $ do
-    describe "Basic instance" $
+    describe "Basic instance" $ do
       it "any type Has itself" $ do
         let exFoo = extract baseFooEnv
         exFoo `shouldBe` baseFooEnv
+      it "any type updates itself" $ do
+        let baseFooEnv' = update fooEnvTwice baseFooEnv
+        baseFooEnv' `shouldBe` baseFooEnvTwice
     describe "Generic instances" $ do
       it "envs have their components" $ do
         let exFoo = extract $ AppEnv baseFooEnv baseBarEnv
         exFoo `shouldBe` baseFooEnv
         let exBar = extract $ AppEnv baseFooEnv baseBarEnv
         exBar `shouldBe` baseBarEnv
+      it "envs update their components" $ do
+        let appEnv' = update fooEnvTwice $ update barEnvCons $ AppEnv baseFooEnv baseBarEnv
+        appEnv' `shouldBe` AppEnv baseFooEnvTwice baseBarEnvCons
       it "tuples have their components" $ do
         let exFoo = extract (baseFooEnv, baseBarEnv)
         exFoo `shouldBe` baseFooEnv
         let exBar = extract (baseFooEnv, baseBarEnv)
         exBar `shouldBe` baseBarEnv
+      it "tuples update their components" $ do
+        let pair' = update fooEnvTwice $ update barEnvCons $ (baseFooEnv, baseBarEnv)
+        pair' `shouldBe` (baseFooEnvTwice, baseBarEnvCons)
     describe "Should not typecheck" $ do
       it "if there is no such type in the hierarchy" $ shouldNotTypecheck extractMissing
       it "if there is more than one such type in the hierarchy" $ shouldNotTypecheck extractMultiple
